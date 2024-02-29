f4b6492710f1317d6b1d64c8dfc56d98da993bda
bachhungcb <bachhungcb@gmail.com>
2024-02-29 14:35:59 +0700

    week 2 in CA

---
+++ week2/mips2.asm
@@ -0,0 +1,7 @@
+#cau truc chuong trinh cua MIPS tren mars
+
+.data #khai bao du lieu
+str: .asciiz "Hello World"
+
+.text #xu ly chinh, chay cac lenh tren chuong trinh
+	li $t0, 10 # $t0 = 10
---
+++ week2/mips3.asm
@@ -0,0 +1,15 @@
+#Nhap xuat chuoi
+
+#nhan gia tri truc tiep
+#$so = 10
+#$s1 = 10
+
+
+#Nhap gia tri truc tiep tu ban phim
+#$s0 = 10
+#SYSCALL 
+.data
+.text
+
+	#li $s0, 10 # $s0 = 10
+	#add $s1, $zero, 15 # $s1 = 0 + 15 = 15
