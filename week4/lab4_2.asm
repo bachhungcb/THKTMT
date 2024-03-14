e9f8c88b47f7348a25723fbf528b029696aa4260
bachhungcb <bachhungcb@gmail.com>
2024-03-14 15:38:05 +0700

    update week 4 in THKTMT

--- week4/lab4_2.asm
+++ week4/lab4_2.asm
@@ -1,4 +1,5 @@
 #Laboratory Exercise 4, Home Assignment 2
+
 .text
 	li $s0, 0x12345678 #load test value for these function
 	xor $t0, $s0, $s0 #Clear $s0
--- week4/lab4_5.asm
+++ week4/lab4_5.asm
@@ -1,6 +1,16 @@
 #Laboratory Exercise 4, Assignment 5
 
 .text
-	li $s0, 5
-	li $s2, 2
-	sllv $s0, $s0, $s2
+	li $s0, 4	#a
+	li $s1, 32	#power of 2 that will be used to multiple with a
+	li $s2, 1	#an index used in loop
+loop:
+	# for(int $s2 = 1; $s2 <= $s1; $s2 *= 2)
+	#	$t0++;
+	#This loop is used to find the pow of 2 of $s1
+	sll $s2, $s2, 1		
+	addi $t0, $t0, 1	
+	beq  $s2, $s1, L1
+	j loop
+L1:
+	sllv $s3, $s0, $t0 
